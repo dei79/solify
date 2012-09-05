1c6e4c20647ba5f9db1a2543651825a979a33517
dei79 <dirk.eisenberg@gmail.com>
2012-09-06 00:18:40 +0200

    initial version

---
+++ chef/control-cookbooks/main/recipes/default.rb
@@ -0,0 +1,40 @@
+package "git-core"
+package "zsh"
+
+rbenv_ruby "1.9.3-p0" do
+  action :install
+end
+
+include_recipe "nginx::source"
+
+user node[:user][:name] do
+  password node[:user][:password]
+  gid "admin"
+  home "/home/#{node[:user][:name]}"
+  supports :manage_home => true
+  shell "/bin/zsh"
+end
+
+template "/home/#{node[:user][:name]}/.zshrc" do
+  source "zshrc.erb"
+  owner node[:user][:name]
+end
+
+directory "/home/#{node[:user][:name]}/example" do
+  owner node[:user][:name]
+end
+
+file "/home/#{node[:user][:name]}/example/index.html" do
+  owner node[:user][:name]
+  content "<h1>Hello World!</h1>"
+end
+
+file "#{node[:nginx][:dir]}/sites-available/example" do
+  content "server
+           {
+              listen 8080;
+              root /home/#{node[:user][:name]}/example;
+           }"
+end
+
+nginx_site "example"
